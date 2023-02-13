b6149c01c1d57c13528c17946beb14748ffbbcf8
xNnennax <114600456+xNnennax@users.noreply.github.com>
2023-02-13 12:22:33 -0500

    Add files via upload

---
+++ coin_game.py
@@ -0,0 +1,16 @@
+WIDTH = 400
+HEIGHT = 400
+score = 0
+game_over = False
+fox = Actor("fox")
+fox.pos = 100, 100
+
+coin = Actor("coin")
+coin.pos = 200, 200
+
+def draw():
+    screen.fill("green")
+    fox.draw()
+    coin.draw()
+   screen.draw.text("Score: " + str(score), color="black", topleft=(10, 10))
+
